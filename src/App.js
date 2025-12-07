ef115e4c06cf81712b4d2bbdbc4d07194a073d91
Joanna Czub <jk.czub@student.uw.edu.pl>
2025-12-07 19:49:43 +0100

    Loading and NoResults added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+import Error from './Error';
+import Loading from './Loading'
+import { NoResults } from './NoResults';
 
-function App() {
-  return (
+export const App = () => (
+  <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NoResults />
     </div>
-  );
-}
+  </ThemeProvider>
+);
 
 export default App;
