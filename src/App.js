eb0dce660f39291b8eef9ad7863d89b2050e45bd
Joanna Czub <jk.czub@student.uw.edu.pl>
2025-12-15 10:17:05 +0100

    MovieList deleted

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import theme from './theme';
 import MoviePage from './MoviePage';
-import MovieList from './MovieList';
 
 export const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     <div className="App">
-      <MovieList />
+  <MoviePage />
     </div>
   </ThemeProvider>
 );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from '../src/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
     <App />
-    </Provider>
   </React.StrictMode>
 );
 
